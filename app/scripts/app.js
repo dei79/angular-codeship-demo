96d37769c42b9e1e8055f7c56aed1443ef2f40a0
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2014-12-20 22:12:45 +0100

    intial commot

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ajsApp
+ * @description
+ * # ajsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ajsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
